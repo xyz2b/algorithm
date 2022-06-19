d81764727f40f92418b8aa8e0adc6fa1d44e29e9
xyz2b <jiaohongyua@gmail.com>
2022-06-19 13:13:28 +0800

    p508 not right solution

---
+++ src/main/java/leetcode/p508/Solution.java
@@ -0,0 +1,62 @@
+package leetcode.p508;
+
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+public class Solution {
+    public int[] findFrequentTreeSum(TreeNode root) {
+        List<Integer> rst = new ArrayList<Integer>();
+        // key为元素和，value为这个元素和出现了几次
+        Map<Integer, Integer> sum = new HashMap<Integer, Integer>();
+
+        treeSum(root, sum);
+
+        int maxCount = 0;
+        for(Map.Entry<Integer, Integer> entry: sum.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                rst.clear();
+                rst.add(entry.getKey());
+            } else if (entry.getValue() == maxCount) {
+                rst.add(entry.getValue());
+            }
+        }
+
+        return null;
+    }
+
+    // 计算以node为根的树的所有元素和
+    public int treeSum(TreeNode node, Map<Integer, Integer> sum) {
+        if (node == null) {
+            return 0;
+        }
+
+        int rightTreeSum = treeSum(node.right, sum);
+        int leftTreeSum = treeSum(node.left, sum);
+        int treeSum = rightTreeSum + leftTreeSum + node.val;
+
+        if (sum.containsKey(treeSum)) {
+            sum.put(treeSum, sum.get(treeSum) + 1);
+        } else {
+            sum.put(treeSum, 1);
+        }
+
+        return treeSum;
+    }
+}
