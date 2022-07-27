cf553e4f06d13d782149cd9c7431cf70729e3e1d
xyz2b <jiaohongyua@gmail.com>
2022-07-27 22:56:28 +0800

    p592

--- src/main/java/leetcode/p592/Solution.java
+++ src/main/java/leetcode/p592/Solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    public String fractionAddition(String expression) {
+    public String fractionAddition1(String expression) {
 
         List<Integer> numerators = new ArrayList<>();
         List<Integer> denominators = new ArrayList<>();
@@ -66,9 +66,59 @@ public class Solution {
         return "";
     }
 
+    // 对于两个分数 x1/y1和x2/y2，他们相加的结果是 (x1*y2+x2*y1) / (y1*y2)
+    public String fractionAddition(String expression) {
+        long denominator = 0, numerator = 1; // 累加之后的分子和分母，可以看成x1和y1
+        int index = 0, n = expression.length();
+
+        while (index < n) {
+            int denominator1 = 0, sign = 1;
+            // 读取分子，可以看成x2
+            if (expression.charAt(index) == '-' || expression.charAt(index) == '+') {   // 读取分子的符号
+                sign = expression.charAt(index) == '-' ? -1 : 1;
+                index++;
+            }
+            while (index < n && Character.isDigit(expression.charAt(index))) {  // 读取分子的数值
+                denominator1 = denominator1 * 10 + expression.charAt(index) - '0';
+                index++;
+            }
+            denominator1 = sign * denominator1;
+            index++;
+
+            // 读取分母，可以看成y2
+            long numerator1 = 0;
+            while (index < n && Character.isDigit(expression.charAt(index))) {  // 读取分母的数值，分母都是正的
+                numerator1 = numerator1 * 10 + expression.charAt(index) - '0';
+                index++;
+            }
+
+            // 相加之后的分子，即 (x1*y2+x2*y1)
+            denominator = denominator * numerator1 + denominator1 * numerator;
+            // 相加之后的分母，即 (y1*y2)
+            numerator *= numerator1;
+        }
+
+        if(denominator == 0) {
+            return "0/1";
+        }
+        long g = gcd(Math.abs(denominator), numerator); // 获取最大公约数
+        return Long.toString(denominator / g) + "/" + Long.toString(numerator / g);
+    }
+
+    // 求最大公约数
+    public long gcd(long a, long b) {
+        long remainder = a % b;
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            remainder = a % b;
+        }
+        return b;
+    }
+
     public static void main(String[] args) {
         String expression = "-10/3-11/2";
         Solution solution = new Solution();
-        solution.fractionAddition(expression);
+        solution.fractionAddition1(expression);
     }
 }
