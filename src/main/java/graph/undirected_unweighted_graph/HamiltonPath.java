cc7668c912c35c86b685e6874ece5e7c4d886ae5
xyz2b <jiaohongyua@gmail.com>
2022-12-02 09:58:44 +0800

    hamilton path

--- src/main/java/graph/undirected_unweighted_graph/HamiltonPath.java
+++ src/main/java/graph/undirected_unweighted_graph/HamiltonPath.java
@@ -2,29 +2,32 @@ package graph.undirected_unweighted_graph;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Stack;
 
-public class HamiltonLoop {
+public class HamiltonPath {
     private Graph G;
     private boolean[] visited;
     private int[] pre;
     // 哈密尔顿回路的最后一个顶点
     private int end;
 
-    public HamiltonLoop(Graph G) {
+    private int s;
+
+    public HamiltonPath(Graph G, int s) {
         this.G = G;
         visited = new boolean[G.V()];
         pre = new int[G.V()];
         end = -1;
-        dfs(0, G.V(), 0);
+        this.s = s;
+        dfs(s, G.V(), s);
     }
 
-    // v为当前递归的顶点，left为剩下未访问的顶点数，返回值是是否存在哈密尔顿回路
+    // v为当前递归的顶点，left为剩下未访问的顶点数，返回值是是否存在哈密尔顿路径
     private boolean dfs(int v, int left, int parent) {
         visited[v] = true;
         pre[v] = parent;
         left--;
-        if (left == 0 && G.hasEdge(v, 0)) { // 通过v可以回到了原点0，并且所有顶点都被访问过了，就找到了哈密尔顿回路
+
+        if (left == 0 /* && G.hasEdge(v, 0) */) { // 只需要保证所有顶点都被访问过了，不需要通过v再回到了原点s，就找到了哈密尔顿路径
             end = v;
             return true;
         }
@@ -33,13 +36,13 @@ public class HamiltonLoop {
             if(!visited[w]) {
                 if(dfs(w, left, v)) return true;
             }
-//            else if (w == 0 && left == 0) { // 通过v回到了原点0，并且所有顶点都被访问过了，就找到了哈密尔顿回路
+//            else if (/* w == s && */ left == 0) { // 只需要保证所有顶点都被访问过了，不需要通过v再回到了原点s，就找到了哈密尔顿路径
 //                end = v;
 //                return true;
 //            }
         }
         // 回溯
-        // 从顶点v开始找不到哈密尔顿回路，那么就回退到v的父亲节点，即退出v的递归，返回上一层
+        // 从顶点v开始找不到哈密尔顿路径，那么就回退到v的父亲节点，即退出v的递归，返回上一层
         // 此时需要将顶点v置为未访问的
         visited[v] = false;
         // 其实这里不需要left++，因为回退到递归上一层时候，上一层的left是不受下一层子递归的left影响的，因为值传递
@@ -52,11 +55,11 @@ public class HamiltonLoop {
         if(end == -1) return path;
 
         int cur = end;
-        while (cur != 0) {
+        while (cur != s) {
             path.add(cur);
             cur = pre[cur];
         }
-        path.add(0);
+        path.add(s);
 
         Collections.reverse(path);
 
@@ -64,12 +67,8 @@ public class HamiltonLoop {
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph("g3.txt");
-        HamiltonLoop hl = new HamiltonLoop(g);
-        System.out.println(hl.path());
-
-        Graph g2 = new Graph("g4.txt");
-        HamiltonLoop hl2 = new HamiltonLoop(g2);
-        System.out.println(hl2.path());
+        Graph g = new Graph("g5.txt");
+        HamiltonPath hl = new HamiltonPath(g, 1);
+        System.out.println(hl.path());;
     }
 }
