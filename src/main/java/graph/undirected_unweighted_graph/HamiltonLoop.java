b1e4532db56ea323ef4b50554d52098c04f64805
xyz2b <jiaohongyua@gmail.com>
2022-12-05 09:35:58 +0800

    HamiltonLoop state compression

--- src/main/java/graph/undirected_unweighted_graph/HamiltonLoop.java
+++ src/main/java/graph/undirected_unweighted_graph/HamiltonLoop.java
@@ -1,19 +1,22 @@
 package graph.undirected_unweighted_graph;
 
+import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
 
 public class HamiltonLoop {
     private Graph G;
-    private boolean[] visited;
+//    private boolean[] visited;
+    // 状态压缩，使用int的一位标识某个顶点是否已经访问过
+    private int visited;
     private int[] pre;
     // 哈密尔顿回路的最后一个顶点
     private int end;
 
     public HamiltonLoop(Graph G) {
         this.G = G;
-        visited = new boolean[G.V()];
+        visited = 0;
         pre = new int[G.V()];
         end = -1;
         dfs(0, G.V(), 0);
@@ -21,7 +24,8 @@ public class HamiltonLoop {
 
     // v为当前递归的顶点，left为剩下未访问的顶点数，返回值是是否存在哈密尔顿回路
     private boolean dfs(int v, int left, int parent) {
-        visited[v] = true;
+//        visited[v] = true;
+        visited += (1 << v);
         pre[v] = parent;
         left--;
         if (left == 0 && G.hasEdge(v, 0)) { // 通过v可以回到了原点0，并且所有顶点都被访问过了，就找到了哈密尔顿回路
@@ -30,7 +34,7 @@ public class HamiltonLoop {
         }
 
         for(int w : G.adj(v)) {
-            if(!visited[w]) {
+            if((visited & (1 << w)) == 0 /* !visited[w] */) {
                 if(dfs(w, left, v)) return true;
             }
 //            else if (w == 0 && left == 0) { // 通过v回到了原点0，并且所有顶点都被访问过了，就找到了哈密尔顿回路
@@ -41,7 +45,8 @@ public class HamiltonLoop {
         // 回溯
         // 从顶点v开始找不到哈密尔顿回路，那么就回退到v的父亲节点，即退出v的递归，返回上一层
         // 此时需要将顶点v置为未访问的
-        visited[v] = false;
+//        visited[v] = false;
+        visited -= (1 << v);
         // 其实这里不需要left++，因为回退到递归上一层时候，上一层的left是不受下一层子递归的left影响的，因为值传递
 //        left++;
         return false;
