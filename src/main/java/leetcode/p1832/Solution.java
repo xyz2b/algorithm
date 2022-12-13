a10b24bd6225391e17a2bed6273b0b18cf38e8b6
xyz2b <jiaohongyua@gmail.com>
2022-12-13 11:08:28 +0800

    p1832

--- src/main/java/leetcode/p1832/Solution.java
+++ src/main/java/leetcode/p1832/Solution.java
@@ -17,6 +17,21 @@ public class Solution {
         return true;
     }
 
+    // 使用数组记录每种字符是否出现仍然需要 O(C) 的空间复杂度。由于字符集仅有 26 个，
+    // 我们可以使用一个长度为 26 的二进制数字来表示字符集合，这个二进制数字使用 32 位带符号整型变量即可。
+    public boolean checkIfPangram2(String sentence) {
+        int letter = 0;
+
+        for(int i = 0 ; i < sentence.length(); i++) {
+            int c = sentence.charAt(i) - 'a';
+            // 将对应索引位置上的二进制置为1，如果26个位置上都是1，则表明为全字母
+            letter |= (1 << c);
+        }
+
+        // 26位全1: (1 << 26) - 1
+        return letter == (1 << 26) - 1;
+    }
+
     public static void main(String[] args) {
         String sentence = "leetcode";
         Solution solution = new Solution();
