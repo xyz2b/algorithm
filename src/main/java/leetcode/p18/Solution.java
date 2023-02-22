7bb8a434ae08f499d7eed466a8c83cf99ad936f3
xyz2b <jiaohongyua@gmail.com>
2023-02-22 22:50:29 +0800

    p18 two pointer

--- src/main/java/leetcode/p18/Solution.java
+++ src/main/java/leetcode/p18/Solution.java
@@ -29,10 +29,45 @@ public class Solution {
         return new ArrayList<List<Integer>>(rstSet);
     }
 
+    // 双指针，threeSum再加一重循环
+    public List<List<Integer>> fourSum2(int[] nums, int target) {
+        List<List<Integer>> rst = new ArrayList<>();
+
+        // 排序
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+
+            for(int j = i + 1; j < nums.length; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue; // 去重
+
+                // 双指针
+                int l = j + 1, r = nums.length - 1;
+                while (l < r) {
+                    long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
+                    if(sum > target) {
+                        r--;
+                    } else if (sum < target) {
+                        l++;
+                    } else { // sum == target
+                        rst.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[l], nums[r])));
+                        while (l < r && nums[l] == nums[l + 1]) l++;    // 去重
+                        while (l < r && nums[r] == nums[r - 1]) r--;    // 去重
+                        l++;
+                        r--;
+                    }
+                }
+            }
+        }
+
+        return rst;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,0,-1,0,-2,2};
-        int target = 0;
+        int[] nums = {1000000000,1000000000,1000000000,1000000000};
+        int target = -294967296;
         Solution solution = new Solution();
-        System.out.println(solution.fourSum(nums, target));
+        System.out.println(solution.fourSum2(nums, target));
     }
 }
