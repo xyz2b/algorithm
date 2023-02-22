7fe38aed7340784c3fb99fcdbbd1080f26ae13c2
xyz2b <jiaohongyua@gmail.com>
2023-02-22 17:47:38 +0800

    p15 two pointer

--- src/main/java/leetcode/p15/Solution.java
+++ src/main/java/leetcode/p15/Solution.java
@@ -26,6 +26,37 @@ public class Solution {
         return new ArrayList<List<Integer>>(rstSet);
     }
 
+    public List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> rst = new ArrayList<>();
+
+        // 排序之后使用双指针
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+
+            // 双指针
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else { // sum == 0
+                    rst.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
+                    // 去重
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return rst;
+    }
 
     public static void main(String[] args) {
         int[] nums = {0,0,1};
