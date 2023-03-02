071f139102070562799534358a534e6a5f7f90d3
xyz2b <jiaohongyua@gmail.com>
2023-03-02 10:58:00 +0800

    p2

--- src/main/java/leetcode/p2/Solution.java
+++ src/main/java/leetcode/p2/Solution.java
@@ -60,11 +60,43 @@ public class Solution {
         return rstHead.next;
     }
 
+    public ListNode addTwoNumbersOptimizeCode(ListNode l1, ListNode l2) {
+        ListNode rstHead = new ListNode();
+        ListNode rst = rstHead;
+
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+
+        int carry = 0;
+        while (cur1 != null || cur2 != null) {
+            int x = cur1 != null ? cur1.val : 0;
+            int y = cur2 != null ? cur2.val : 0;
+            int sum = x + y + carry;
+            int quotient = sum / 10;    // 商
+            int remainder = sum % 10;   // 余数
+
+            rst.next = new ListNode(remainder);
+            rst = rst.next;
+
+            carry = quotient;   // 进位
+
+            if (cur1 != null) cur1 = cur1.next;
+            if (cur2 != null) cur2 = cur2.next;
+        }
+
+        if(carry != 0) {
+            rst.next = new ListNode(carry);
+        }
+
+        return rstHead.next;
+    }
+
+
     public static void main(String[] args) {
-        ListNode l1 = LinkedList.createLinkedList(new int[] {9,9,9,9,9,9,9});
-        ListNode l2 = LinkedList.createLinkedList(new int[] {9,9,9,9});
+        ListNode l1 = LinkedList.createLinkedList(new int[] {2,4,3});
+        ListNode l2 = LinkedList.createLinkedList(new int[] {5,6,4});
 
         Solution solution = new Solution();
-        System.out.println(LinkedList.linkedListToString(solution.addTwoNumbers(l1, l2)));
+        System.out.println(LinkedList.linkedListToString(solution.addTwoNumbersOptimizeCode(l1, l2)));
     }
 }
