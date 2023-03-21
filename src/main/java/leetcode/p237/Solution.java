a359b398bdf8240bbc75668714af0783050d47a1
xyz2b <jiaohongyua@gmail.com>
2023-03-21 18:06:41 +0800

    p237

--- src/main/java/leetcode/p237/Solution.java
+++ src/main/java/leetcode/p237/Solution.java
@@ -4,17 +4,22 @@ import leetcode.p206.ListNode;
 import leetcode.p206.LinkedList;
 
 public class Solution {
-    // 将待删除节点后面所有节点的值向前平移一次，然后删除最后一个节点(尾结点)即可
+    // 将待删除节点后面一个节点的值复制到待删除节点的val中，然后删除待删除节点后一个节点即可
     public void deleteNode(ListNode node) {
-        ListNode cur = node;
-        // 因为要删除最后一个节点，所以需要知道最后一个节点的前一个节点，所以这里循环只到cur.next.next == null时，cur就是待删除尾结点的前一个节点，cur.next就是需要删除的那个尾结点
-        while (cur.next.next != null) {
-            ListNode next = cur.next;
-            cur.val = next.val;
-            cur = next;
+        if(node == null) {
+            return;
         }
 
-        cur.val = cur.next.val;
-        cur.next = null;
+        if(node.next == null) {
+            node = null;
+            return;
+        }
+
+        node.val = node.next.val;
+        ListNode delNode = node.next;
+        node.next = delNode.next;
+        delNode.next = null;
+
+        return;
     }
 }
