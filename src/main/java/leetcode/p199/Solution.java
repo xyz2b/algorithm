326b1ea1aae408b336ba26d058c69bf9bdb8767e
xyz2b <jiaohongyua@gmail.com>
2023-03-29 09:03:21 +0800

    p199

--- src/main/java/leetcode/p199/Solution.java
+++ src/main/java/leetcode/p199/Solution.java
@@ -9,6 +9,7 @@ import java.util.Queue;
 
 public class Solution {
     // 层序遍历，队列
+    // 结果是 每一层最右边的节点，每一层从左向右遍历的最后一个节点
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> rst = new ArrayList<>();
         if(root == null) return rst;
