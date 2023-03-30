b0aa79ab66ccfb0c47aab25559115e9423cc22c0
xyz2b <jiaohongyua@gmail.com>
2023-03-30 15:34:57 +0800

    p347

---
+++ src/main/java/leetcode/p347/Solution.java
@@ -0,0 +1,56 @@
+package leetcode.p347;
+
+import java.util.*;
+
+public class Solution {
+    // 前k大元素，最小堆
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        // 统计频率
+        for(int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // 优先队列，最小堆，前k大元素
+        Queue<Map.Entry<Integer, Integer>> entries = new PriorityQueue<>(k, (Map.Entry<Integer, Integer> entry1, Map.Entry<Integer, Integer> entry2) -> {
+            int count1 = entry1.getValue();
+            int count2 = entry2.getValue();
+            return count1 - count2;
+        });
+
+        int index = 0;
+        for(Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if(index < k) {
+                entries.add(entry);
+            } else {
+                Map.Entry<Integer, Integer> minEntry = entries.peek();
+
+                if(entry.getValue() > minEntry.getValue()) {    // 如果当前元素比堆顶元素大(比最小的大)，就把堆顶元素删除，将当前元素加入堆
+                    entries.poll();
+                    entries.add(entry);
+                }
+            }
+            index++;
+        }
+
+        int[] rst = new int[entries.size()];
+        int i = 0;
+        while (!entries.isEmpty()) {
+            Map.Entry<Integer, Integer> entry = entries.poll();
+            rst[i] = entry.getKey();
+            i++;
+        }
+
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1};
+        int k = 2;
+        Solution solution = new Solution();
+        int[] rst = solution.topKFrequent(nums, k);
+        for(int i = 0; i < rst.length; i++) {
+            System.out.println(rst[i]);
+        }
+    }
+}
