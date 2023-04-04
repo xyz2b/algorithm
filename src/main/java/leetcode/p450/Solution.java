2898a0a9c3978e32847edebc5260595211073482
xyz2b <jiaohongyua@gmail.com>
2023-04-04 20:19:08 +0800

    p450

--- src/main/java/leetcode/p450/Solution.java
+++ src/main/java/leetcode/p450/Solution.java
@@ -4,63 +4,61 @@ import leetcode.p144.TreeNode;
 
 public class Solution {
     // 找到待删除节点的，前驱或后继，替换掉当前要删除的元素
-    // 前驱左子树的右子树的右子树一直到叶子节点
-    // 后继右子树的左子树左子树一直到叶子节点
+    // 前驱左子树的右子树的右子树一直到叶子节点（就是左子树中的最大节点）
+    // 后继右子树的左子树左子树一直到叶子节点（就是右子树中的最小节点）
 
+    // 返回以root为根的二叉树中，删除val为key的node以后新的树的根
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null) {
-            return root;
+            return null;
         }
 
         if(root.val == key) {
             if(root.left == null) {
-
-            }
-        } else if (key < root.val) {
-            return deleteNode(root.left, key);
-        } else { // key > root.val
-            return deleteNode(root.right, key);
-        }
-    }
-
-    // 返回以root为根的二叉树中，删除val为key的node以后新的树的根
-    public TreeNode delete(TreeNode node, int key) {
-        if(node == null) {
-            return null;
-        }
-
-        if(node.val == key) {
-            if(node.left == null) {
-                TreeNode rightNode = node.right;
-                node.right = null;
+                TreeNode rightNode = root.right;
+                root.right = null;
                 return rightNode;
-            } else if (node.right == null) {
-                TreeNode leftNode = node.left;
-                node.left = null;
+            } else if (root.right == null) {
+                TreeNode leftNode = root.left;
+                root.left = null;
                 return leftNode;
             } else { // node.left != null && node.right != null
-                // 后继
-                // 后继右子树的左子树左子树一直到叶子节点
-                TreeNode successor = successor(node.right);
-
-                successor.left = node.left;
-                successor.right = node.right;
-                node.left = null;
-                node.right = null;
+                // 找到后继，替换掉当前root节点
+                // 当前节点的后继: 右子树的左子树左子树一直到叶子节点（就是右子树中的最小节点）
+                TreeNode successor = findMin(root.right);
+                successor.right = delMin(root.right);
+                successor.left = root.left;
+                root.left = null;
+                root.right = null;
                 return successor;
             }
-        } else if (key < node.val) {
-            return deleteNode(node.left, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
         } else { // key > root.val
-            return deleteNode(node.right, key);
+            root.right = deleteNode(root.right, key);
         }
+
+        return root;
     }
 
-    public TreeNode successor(TreeNode node) {
+    // 返回以node为根的二叉树中的最小值
+    private TreeNode findMin(TreeNode node) {
         if(node.left == null) {
             return node;
         }
 
-        return successor(node.left);
+        return findMin(node.left);
+    }
+
+    // 删除以node为根的二叉树中的最小值，然后返回新的根
+    private TreeNode delMin(TreeNode node) {
+        if(node.left == null) {
+            TreeNode rightNode = node.right;
+            node.right = null;
+            return rightNode;
+        }
+
+         node.left = delMin(node.left);
+        return node;
     }
 }
