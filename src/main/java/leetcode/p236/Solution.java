3d14cc457294e5824354fca498b55b277660374d
xyz2b <jiaohongyua@gmail.com>
2023-04-06 09:51:47 +0800

    p236

--- src/main/java/leetcode/p236/Solution.java
+++ src/main/java/leetcode/p236/Solution.java
@@ -49,6 +49,61 @@ public class Solution {
         return path;
     }
 
+    /**
+     * 1.node的左子树包含p，而node的右子树包含q，则node就是最近公共祖先
+     * 2.node的右子树包含p，而node的左子树包含q，则node就是最近公共祖先
+     *
+     * 3.node等于p，而且node的左或右子树包含q，则node就是最近公共祖先
+     * 4.node等于q，而且node的左或右子树包含p，则node就是最近公共祖先
+     * 5.node等于p又等于q，则node就是最近公共祖先
+     *
+     * 6.p和q都在node左子树中，则在左子树中寻找
+     * 7.p和q都在node右子树中，则在右子树中寻找
+     *
+     * 综上所述，只要p和q不在node的同一边，则node就是最近公共祖先
+     * */
+    private TreeNode rst;
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        bfs(root, p, q);
+        return rst;
+    }
+
+    // 以node为根的二叉树中是否包含p或q（同时包含，或者只包含一个）
+    private boolean bfs(TreeNode node, TreeNode p, TreeNode q) {
+        if(node == null) return false;
+
+        boolean left = bfs(node.left, p, q);
+        boolean right = bfs(node.right, p, q);
+
+        if(left && right) { // 1 和 2
+            rst = node;
+        }
+
+        if(node.val == p.val && (left || right)) { // 3
+            rst = node;
+        }
+
+        if(node.val == q.val && (left || right)) { // 4
+            rst = node;
+        }
+
+        if(node.val == p.val && node.val == q.val) { // 5
+            rst = node;
+        }
+
+
+        if(left || right) { // 以node为根的二叉树的右子树或者左子树中，包含p或q
+            return true;
+        }
+
+        if(node.val == p.val || node.val == q.val) {    // node节点本身等于p或q
+            return true;
+        }
+
+        // 以node为根的二叉树中不包含p或q
+        return false;
+    }
+
     public static void main(String[] args) {
 
     }
