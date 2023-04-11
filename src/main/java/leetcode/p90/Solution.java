bf224e4e57cec02279bff7070a7c1640bfa880fc
xyz2b <jiaohongyua@gmail.com>
2023-04-11 18:19:05 +0800

    p90

--- src/main/java/leetcode/p90/Solution.java
+++ src/main/java/leetcode/p90/Solution.java
@@ -1,12 +1,16 @@
 package leetcode.p90;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     private List<List<Integer>> rst = new ArrayList<>();
+    private boolean[] used;
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        used = new boolean[nums.length];
+        Arrays.sort(nums);
         subsets(nums, 0, new ArrayList<>());
         return rst;
     }
@@ -18,13 +22,16 @@ public class Solution {
         // 给出的数字集合中有重复数字
         for(int i = startIndex; i < nums.length; i++) {
             // 因为数组中有重复的数字，所以需要去除重复
-            // 这里就约定一个规则来去除重复: 连续相同的值，只有从左到右的第一个值能被使用
-            if(i > 0 && nums[i] == nums[i-1]) {
+            // 这里就约定一个规则来去除重复: 连续相同的值，只有从左到右的第一个值能被使用，因为要记录连续重复的值，第一个值有没有被使用过，所以需要used数组
+            if(i > 0 && nums[i] == nums[i-1] && !used[i - 1]) {
                 continue;
             }
+
             s.add(nums[i]);
+            used[i] = true;
             subsets(nums, i + 1, s);
             s.remove(s.size() - 1);
+            used[i] = false;
         }
     }
 
