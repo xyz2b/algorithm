229564d226b6762126d9c4348cf353c459b4546d
xyz2b <jiaohongyua@gmail.com>
2023-04-14 22:21:28 +0800

    p120

--- src/main/java/dynamicprogramming/p120/Solution.java
+++ src/main/java/dynamicprogramming/p120/Solution.java
@@ -50,6 +50,26 @@ public class Solution {
         return n;
     }
 
+    // 动态规划 - 自下而上，从小数据到大数据
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        List<List<Integer>>  memo = new ArrayList<>();
+
+        for(List<Integer> t : triangle) {
+            List<Integer> l  = new ArrayList<>(t);
+            memo.add(l);
+        }
+
+        // (index, i)的最小和路径等于 (index + 1, i)最小路径和 与 (index + 1, i + 1)最小路径和中的较小值 + (index, i)的值
+        for(int i = triangle.size() - 2; i >= 0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                int min = Math.min(memo.get(i + 1).get(j), memo.get(i + 1).get(j + 1));
+                memo.get(i).set(j, min + triangle.get(i).get(j));
+            }
+        }
+
+        return memo.get(0).get(0);
+    }
+
     public static void main(String[] args) {
         int[][] t = {{2},{3,4},{6,5,7},{4,1,8,3}};
         List<List<Integer>> triangle = new ArrayList<>();
@@ -61,6 +81,6 @@ public class Solution {
             triangle.add(n);
         }
         Solution solution = new Solution();
-        System.out.println(solution.minimumTotal(triangle));
+        System.out.println(solution.minimumTotal2(triangle));
     }
 }
