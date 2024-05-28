86a2813a16e1617ca53c4193fe6fd13632fce981
xyz2b <jiaohongyua@gmail.com>
2024-05-28 09:56:35 +0800

    p2951

---
+++ src/main/java/leetcode/p2951/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p2951;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> ret = new ArrayList<>();
+        for(int i = 1; i < mountain.length - 1; i++) {
+            if(mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) {
+                ret.add(i);
+            }
+        }
+
+        return ret;
+    }
+}
