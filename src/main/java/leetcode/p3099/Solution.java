a66f6a3b294f76fe2b2e04e488764677104d8dcc
xyz2b <jiaohongyua@gmail.com>
2024-07-03 09:30:14 +0800

    p3099

---
+++ src/main/java/leetcode/p3099/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p3099;
+
+public class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int temp = x;
+        // 分解数位
+        int sum = 0;
+        while (temp != 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        return x % sum == 0 ? sum : -1;
+    }
+}
