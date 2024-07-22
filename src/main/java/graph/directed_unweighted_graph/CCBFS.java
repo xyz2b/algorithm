0a0fae64fe49c7beafe370562548d8af06987067
xyz2b <jiaohongyua@gmail.com>
2024-07-22 09:59:48 +0800

    directed unweighted graph not ccbfs and ccdfs

--- src/main/java/graph/directed_unweighted_graph/CCBFS.java
+++ src/main/java/graph/directed_unweighted_graph/CCBFS.java
@@ -6,36 +6,29 @@ import java.util.Arrays;
 import java.util.Queue;
 
 // 同无向图
-// 联通分量，即一个图中不连通的子图的个数（联通：可以从一个顶点经过若干条边到达另一个顶点）
+// 计算从某个节点开始所能到达的节点数目
 public class CCBFS {
     private Graph G;
     private int[] visited;
-    // 联通分量的个数
-    private int ccCount = 0;
-    // 每个联通分量的顶点数
     private ArrayList<Integer> ccVCount = new ArrayList<>();
-
 
     public CCBFS(Graph G) {
         this.G = G;
         visited = new int[G.V()];
-        // -1表示还未遍历过，相同数值的顶点之间是存在连接的，不同数值的顶点之间不存在连接
-        Arrays.fill(visited, -1);
+
 
         for(int v = 0; v < G.V(); v++) {
-            if(visited[v] == -1) {
-                int vCount = bfs(v, ccCount);
-                // 每次进入该逻辑都是一个新的联通分量
-                ccVCount.add(vCount);
-                ccCount++;
-            }
+            // -1表示还未遍历过
+            Arrays.fill(visited, -1);
+            int vCount = bfs(v);
+            ccVCount.add(vCount);
         }
     }
 
-    private int bfs(int s, int ccid) {
+    private int bfs(int s) {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.add(s);
-        visited[s] = ccid;
+        visited[s] = 1;
         int vCount = 1;
 
         while (!queue.isEmpty()) {
@@ -44,7 +37,7 @@ public class CCBFS {
             for(int w : G.adj(v)) {
                 if(visited[w] == -1) {
                     queue.add(w);
-                    visited[w] = ccid;
+                    visited[w] = 1;
                     vCount++;
                 }
             }
@@ -52,53 +45,7 @@ public class CCBFS {
         return vCount;
     }
 
-    // 联通分量的个数
-    public int count() {
-        return ccCount;
-    }
-
-    // 判断两个顶点是否联通
-    public boolean isConnected(int v, int w) {
-        G.validateVertex(v);
-        G.validateVertex(w);
-        return visited[v] == visited[w];
-    }
-
-    // 返回各个联通分量的顶点集合
-    public ArrayList<Integer>[] components() {
-        ArrayList<Integer>[] res = new ArrayList[ccCount];
-        for(int i = 0; i < ccCount; i++) {
-            res[i] = new ArrayList<>();
-        }
-
-        for(int v = 0; v < visited.length; v++) {
-            res[visited[v]].add(v);
-        }
-
-        return res;
-    }
-
     public Iterable<Integer> ccVCount() {
         return ccVCount;
-    }
-
-    public static void main(String[] args) {
-        Graph graph = new Graph("g.txt");
-        CCBFS ccbfs = new CCBFS(graph);
-        for(ArrayList<Integer> cc : ccbfs.components()) {
-            System.out.println(cc);
-        }
-        for(int vCount : ccbfs.ccVCount()) {
-            System.out.println(vCount);
-        }
-        System.out.println("==========================");
-        Graph graph2 = new Graph("g2.txt");
-        CCBFS ccbfs2 = new CCBFS(graph2);
-        for(ArrayList<Integer> cc : ccbfs2.components()) {
-            System.out.println(cc);
-        }
-        for(int vCount : ccbfs2.ccVCount()) {
-            System.out.println(vCount);
-        }
     }
 }
--- src/main/java/graph/directed_unweighted_graph/CCDFS.java
+++ src/main/java/graph/directed_unweighted_graph/CCDFS.java
@@ -4,90 +4,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 // 同无向图
-// 联通分量，即一个图中不连通的子图的个数（联通：可以从一个顶点经过若干条边到达另一个顶点）
+// 计算从某个节点开始所能到达的节点数目
 public class CCDFS {
     private Graph G;
     private int[] visited;
-    // 联通分量的个数
-    private int ccCount = 0;
-    // 每个联通分量的顶点数
     private ArrayList<Integer> ccVCount = new ArrayList<>();
 
     public CCDFS(Graph G) {
         this.G = G;
         visited = new int[G.V()];
-        // -1表示还未遍历过，相同数值的顶点之间是存在连接的，不同数值的顶点之间不存在连接
-        Arrays.fill(visited, -1);
 
         for(int v = 0; v < G.V(); v++) {
-            if(visited[v] == -1) {
-                int vCount = dfs(v, ccCount);
-                // 每次进入该逻辑都是一个新的联通分量
-                ccVCount.add(vCount);
-                ccCount++;
-            }
+            // -1表示还未遍历过
+            Arrays.fill(visited, -1);
+            int vCount = dfs(v);
+            // 每次进入该逻辑都是一个新的联通分量
+            ccVCount.add(vCount);
         }
     }
 
     // v顶点所在联通分量的顶点数
-    private int dfs(int v, int ccid) {
-        visited[v] = ccid;
+    private int dfs(int v) {
+        visited[v] = 1;
         int vCount = 1;
         for(int w : G.adj(v)) {
             if(visited[w] == -1) {
-                vCount += dfs(w, ccid);
+                vCount += dfs(w);
             }
         }
         return vCount;
     }
 
-    // 联通分量的个数
-    public int count() {
-        return ccCount;
-    }
-
-    // 判断两个顶点是否联通
-    public boolean isConnected(int v, int w) {
-        G.validateVertex(v);
-        G.validateVertex(w);
-        return visited[v] == visited[w];
-    }
-
-    // 返回各个联通分量的顶点集合
-    public ArrayList<Integer>[] components() {
-        ArrayList<Integer>[] res = new ArrayList[ccCount];
-        for(int i = 0; i < ccCount; i++) {
-            res[i] = new ArrayList<>();
-        }
-
-        for(int v = 0; v < visited.length; v++) {
-            res[visited[v]].add(v);
-        }
-
-        return res;
-    }
-
     public Iterable<Integer> ccVCount() {
         return ccVCount;
-    }
-
-    public static void main(String[] args) {
-        Graph graph = new Graph("g.txt");
-        CCDFS ccdfs = new CCDFS(graph);
-        for(ArrayList<Integer> cc : ccdfs.components()) {
-            System.out.println(cc);
-        }
-        for(int vCount : ccdfs.ccVCount()) {
-            System.out.println(vCount);
-        }
-        System.out.println("==========================");
-       Graph graph2 = new Graph("g2.txt");
-        CCDFS ccdfs2 = new CCDFS(graph2);
-        for(ArrayList<Integer> cc : ccdfs2.components()) {
-            System.out.println(cc);
-        }
-        for(int vCount : ccdfs2.ccVCount()) {
-            System.out.println(vCount);
-        }
     }
 }
