bcc0229f4ffe543de5b912c20fa4bb6d7774466d
xyz2b <jiaohongyua@gmail.com>
2024-07-31 16:05:24 +0800

    p3111

--- src/main/java/leetcode/p3111/Solution.java
+++ src/main/java/leetcode/p3111/Solution.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Solution {
     public int minRectanglesToCoverPoints(int[][] points, int w) {
-        // x1 x2的差距要在w以内，那每次都取最大值w，然后再遍历下一个可能得矩形开始点x1，再加w，循环往复
+        // x1 x2的差距要在w以内，那每次都取最大值w，然后再遍历下一个可能得矩形开始点x1，再加w，循环往复，每次得到的都是一个可能的矩形
         // y不影响结果，y2可以取x1 x2区间内的点中y的最大值，总能覆盖到区间内的所有的点
 
         // 按照x进行排序
