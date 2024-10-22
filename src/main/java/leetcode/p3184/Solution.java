77557702f6d545b13acd17528772c924f94bf2c0
xyz2b <jiaohongyua@gmail.com>
2024-10-22 11:47:16 +0800

    p3184

---
+++ src/main/java/leetcode/p3184/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p3184;
+
+public class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+        int ret = 0;
+        for(int i = 0; i < hours.length - 1; i++) {
+            for(int j = i + 1; j < hours.length; j++) {
+                if((hours[i] + hours[j]) % 24 == 0) {
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+}
